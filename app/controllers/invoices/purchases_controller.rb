0f472c54ffa682becd44bc47c8715c3a29cba2fa
matsletten <matsletten@gmail.com>
2015-03-16 22:22:46 +0000

    added some invoice and purchase adding function

--- app/controllers/invoices/purchases_controller.rb
+++ app/controllers/invoices/purchases_controller.rb
@@ -1,5 +1,5 @@
-class PurchasesController < ApplicationController
-  before_action :set_purchase, only: [:show, :edit, :update, :destroy]
+class Invoices::PurchasesController < ApplicationController
+#   before_action :set_purchase, only: [:show, :edit, :update, :destroy]
 
   respond_to :html
 
@@ -13,17 +13,30 @@ class PurchasesController < ApplicationController
   end
 
   def new
+#     @purchase = Purchase.new
+#     respond_with(@purchase)
+    @invoice = Invoice.find(params[:invoice_id])
     @purchase = Purchase.new
-    respond_with(@purchase)
   end
 
   def edit
   end
 
   def create
+    @invoice = Invoice.find(params[:invoice_id])
     @purchase = Purchase.new(purchase_params)
-    @purchase.save
-    respond_with(@purchase)
+    @purchase.invoice = @invoice
+#     @purchase.save
+#     respond_with(@purchase)
+    respond_to do |format|
+      if @purchase.save
+        format.html { redirect_to @invoice, notice: 'Purchase was successfully created.' }
+        format.json { render action: 'show', status: :created, location: @invoice }
+      else
+        format.html { render action: 'new' }
+        format.json { render json: @invoice.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   def update
