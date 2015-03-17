39e45fdd3bb8b7da5779e42122ce95e73294387d
matsletten <matsletten@gmail.com>
2015-03-17 00:19:14 +0000

    trying to fix heroku deploy error

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,7 +77,20 @@ Rails.application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
-
+  
+  config.action_mailer.default_url_options = {:host => 'mslettenenterprise.herokuapp.com'}
+  
+  ActionMailer::Base.smtp_settings = {
+  :address          =>  'smtp.sendgrid.net',
+  :port             =>  '587',
+  :authentication   =>  :plain,
+  :user_name        =>  ENV['SENDGRID_USERNAME'],
+  :password         =>  ENV['SENDGRID_PASSWORD'],
+  :domain           => 'heroku.com',
+  :enable_starttls_auto  => true
+}
+  ActionMailer::Base.delivery_method = :smtp
+  
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 end
